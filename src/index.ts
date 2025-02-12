5bc1f8327492b1fb7248c94ac6e40116a0fa2776
PavanCodes05 <pavancodes05@gmail.com>
2025-02-12 21:09:20 +0530

    v1 Route Setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from 'express';
 
 import { ServerConfig } from './config';
+import { v1Routes } from './routes';
 
 const app: Express = express();
+
+app.use('/v1', v1Routes);
 
 app.listen(ServerConfig, () => {
     console.log(`Server is running on the port: ${ServerConfig}`);
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { router as v1Routes } from './v1';
+
+export {
+    v1Routes
+}
