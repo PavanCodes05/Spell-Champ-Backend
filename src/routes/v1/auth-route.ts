e32aabf1f7ad8630370b7907f4e9e2ba165cdf47
PavanCodes05 <pavancodes05@gmail.com>
2025-02-13 20:44:45 +0530

    Add auth route - Signup/Login

---
+++ src/routes/v1/auth-route.ts
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+import { authController } from '../../controllers/index.js'
+
+const router:Router = express.Router();
+
+router.post('/signup', authController.emailSignup);
+router.post('/login', authController.emailLogin);
+
+export default router;
