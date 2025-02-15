c9535493b0a92fc98fc8de8a92c8d6cba6441231
PavanCodes05 <pavancodes05@gmail.com>
2025-02-15 19:48:40 +0530

    Add password to the model

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,9 +1,10 @@
 import { z as zod } from 'zod';
 
 const UserSchema = zod.object({
-    userId: zod.string(),
+    userId: zod.string().default(""),
     name: zod.string().min(1, "Name is required"),
     email: zod.string().email(),
+    password: zod.string().min(6, "Password must be atleast 6 characters"),
     currentGrade: zod.number().min(1, "Grade must be at least 1"),
     trophies: zod.number().nonnegative().default(0)
 });
