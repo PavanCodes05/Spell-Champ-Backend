5fa35a9d538ed8b938f6b9901a558d87902b7b44
PavanCodes05 <pavancodes05@gmail.com>
2025-02-19 20:03:33 +0530

    Add Cloudinary Config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import ServerConfig from './server-config.js';
-import FirebaseConfig from './database-config.js'
+import FirebaseConfig from './database-config.js';
+import cloudinary from './cloudinary-config.js';
 
 export {
     ServerConfig,
-    FirebaseConfig
+    FirebaseConfig,
+    cloudinary
 }
