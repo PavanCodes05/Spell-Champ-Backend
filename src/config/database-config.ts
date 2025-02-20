598985d0cb247a662a448073f32bb3fbcbf17a1b
PavanCodes05 <pavancodes05@gmail.com>
2025-02-20 15:17:17 +0530

    Change Firebase Config File

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -5,13 +5,11 @@ import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    databaseUrl: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
-    mesaurementId: process.env.MEASUREMENT_ID
-}
+};
 
 const app = initializeApp(firebaseConfig);
 
