1b6d3b3984a87f94707d43f0f312c0c29b9c65c7
PavanCodes05 <pavancodes05@gmail.com>
2025-02-23 08:32:41 +0530

    Add Default value to currentGrade

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -5,7 +5,7 @@ const UserSchema = zod.object({
     name: zod.string().min(1, "Name is required"),
     email: zod.string().email(),
     password: zod.string().min(6, "Password must be atleast 6 characters"),
-    currentGrade: zod.number().min(1, "Grade must be at least 1").max(12).optional(),
+    currentGrade: zod.number().min(1, "Grade must be at least 1").max(12).default(1),
     trophies: zod.number().nonnegative().default(0)
 });
 
