5fc21d7d53fb8bf4ebb691171d958322b8ea18ff
PavanCodes05 <pavancodes05@gmail.com>
2025-04-05 11:00:13 +0530

    Categorize grade-routes

--- src/routes/v1/grade-route.ts
+++ src/routes/v1/grade-route.ts
@@ -3,9 +3,13 @@ import { GradeController } from '../../controllers/index.js';
 
 const router: Router = express.Router();
 
+// For Admin Dashboard
 router.post('/create-exercise', GradeController.createExerciseController);
 router.post('/create-grade', GradeController.createGradeController);
+
+// For Mobile App
 router.get('/:grade/exercises', GradeController.getExercisesController);
 router.get('/:grade/quizzes', GradeController.getQuizzesController);
 router.put('/update-grade',GradeController.gradeupdateController);
+
 export default router;
