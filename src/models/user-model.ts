d99c0585696977d3c38389a3bb3e05db800220ab
PavanCodes05 <pavancodes05@gmail.com>
2025-04-05 19:20:40 +0530

    Add All Required Field For Profile-UserModel

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,5 +1,12 @@
 import { z as zod } from 'zod';
 
+const TrophiesSchema = zod.object({
+    gold: zod.number().int().nonnegative().default(0),
+    silver: zod.number().int().nonnegative().default(0),
+    bronze: zod.number().int().nonnegative().default(0)
+});
+
+
 const UserSchema = zod.object({
     docId: zod.string().optional(),
     userId: zod.string().default(""),
@@ -7,9 +14,12 @@ const UserSchema = zod.object({
     email: zod.string().email(),
     password: zod.string().min(6, "Password must be atleast 6 characters"),
     currentGrade: zod.number().min(1, "Grade must be at least 1").max(12).default(1),
-    trophies: zod.number().nonnegative().default(0),
+    exercisesCompleted: zod.number().default(0),
+    quizzesCompleted: zod.number().default(0),
+    completedWords: zod.array(zod.string()).optional(),
+    trophies: TrophiesSchema.default({ gold: 0, silver: 0, bronze: 0 }),
     diamonds: zod.number().nonnegative().default(0),
     token: zod.string().optional()
-});
+}).strict();
 
 export default UserSchema;
