7688c74eb721c288c7c92e612b8959f788f7ff01
PavanCodes05 <pavancodes05@gmail.com>
2025-04-07 05:51:29 +0530

    Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import cors from "cors";
 
 import { ServerConfig } from './config/index.js';
 import apiRoutes from './routes/index.js';
@@ -7,6 +8,13 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+app.use(cors(corsOptions)); 
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
